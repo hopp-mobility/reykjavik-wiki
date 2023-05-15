4958896dc5261384252898ff0403353e20a81907
Aegir Thorsteinsson <aegir@thorsteinsson.is>
2023-05-15 10:57:55 +0000

    Change domain back

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Hopp Reykjavik Wiki',
-  domain: 'wiki.hopp.is',
+  domain: 'reykjavik.hopp.is',
   author: 'Hopp Reykjavík',
 
   // open graph metadata (optional)
